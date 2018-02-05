405737388f389852a4c5c892545f6af7c56626b9
Rohit prasad <rohit.prasad@brainvire.com>
2018-02-05 11:06:26 +0530

    dismiss modal

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,5 @@
 import { Component, ViewChild } from '@angular/core';
-import { Platform, Nav } from 'ionic-angular';
+import { Platform, Nav, IonicApp, MenuController } from 'ionic-angular';
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
 import { SignupPage } from '../pages/signup/signup';
@@ -13,9 +13,12 @@ export class MyApp {
   @ViewChild(Nav) nav: Nav;
   rootPage:any= 'SignupPage';
   loader: any;
-  constructor(platform: Platform,
+  constructor(
     statusBar: StatusBar,
     splashScreen: SplashScreen,
+    private menuCtrl: MenuController,
+    private ionicApp: IonicApp,
+    public platform: Platform,
     public loadingCtrl: LoadingController,
     public storage: Storage) {
     this.presentLoading();
@@ -24,6 +27,65 @@ export class MyApp {
       // Here you can do any higher level native things you might need.
       // statusBar.styleDefault();
       // splashScreen.hide();
+      let ready = true;
+      platform.registerBackButtonAction(() => {
+        let activePortal = ionicApp._loadingPortal.getActive() ||
+           ionicApp._modalPortal.getActive() ||
+           ionicApp._toastPortal.getActive() ||
+           ionicApp._overlayPortal.getActive();
+
+        if (activePortal) {
+           ready = false;
+           activePortal.dismiss();
+           activePortal.onDidDismiss(() => { ready = true; });
+           return;
+        }
+        if (menuCtrl.isOpen()) {
+           menuCtrl.close();
+           return;
+        }
+        if (page && page.isRootPage) {
+                let alert = this.alertCtrl.create({
+                title: 'Exit App',
+                message: 'Are you sure you want to exit?',
+                cssClass: 'backPop',
+                buttons: [
+                  {
+                    text: 'No',
+                    cssClass: 'alert-danger',
+                    role: 'cancel',
+                    handler: () => {
+                      console.log('Cancel clicked');
+                      
+                    }
+                  },
+                  {
+                    text: 'Yes',
+                    handler: () => {
+                      console.log('App exited');
+                      platform.exitApp(); // IF IT'S THE ROOT, EXIT THE APP.
+
+                    }
+                  }
+                ]
+              });
+        } else if (this.nav.canGoBack() || view && view.isOverlay
+        ) {
+          //  Logger.log("popping back");
+           this.nav.pop();
+        } else if (localStorage.getItem('introShown')
+        ) {
+          //  Logger.log("Returning to home page");
+           this.nav.setRoot('SignupPage');
+        } else if (!localStorage.getItem('introShown')) {
+        //  Logger.log("Not yet logged in... exiting");
+           platform.exitApp();
+        } else {
+          //  Logger.log("ERROR with back button handling");
+        }
+
+      }, 1);
+
       this.storage.get('introShown').then((result) => {
  
         if(result){
